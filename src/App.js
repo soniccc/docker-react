38c228e590bea0d8362040ffb32c4665fd2629f1
soniccc <charlie.chahal@gmail.com>
2019-02-04 19:46:28 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+             COME ON YOU SPURS!!<br />
+             YIDARMY!!!!<br />
+             Harry Kane... One of Our Own!!<br />
+             YIDS!!<br />
+             COYS!!<br />
+             DELE!!!!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
